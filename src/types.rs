1155d0fcc1fd6823a3e905f79452377fcb28ec85
ctkrug <ctkrug4501@gmail.com>
2026-07-02 00:52:31 +0000

    feat(types): add Color and PieceKind enums

---
+++ src/types.rs
@@ -0,0 +1,26 @@
+/// Color of a chess piece or the side to move.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Color {
+    White,
+    Black,
+}
+
+impl Color {
+    pub fn opposite(self) -> Color {
+        match self {
+            Color::White => Color::Black,
+            Color::Black => Color::White,
+        }
+    }
+}
+
+/// The six distinct chess piece kinds.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum PieceKind {
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
+}
